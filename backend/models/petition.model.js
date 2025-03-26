a90cab4961f0e0d9eeffa369169a37105f5ec0ce
Favour Benjamin <favemeka146@gmail.com>
2025-03-26 19:25:00 +0100

    Made Petition Schema Strict

--- backend/models/petition.model.js
+++ backend/models/petition.model.js
@@ -2,20 +2,19 @@ import mongoose from "mongoose";
 
 const PetitionSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  phone: { type: String, required: false },
-  email: { type: String, required: false },
-  sex: { type: String, required: false },
-  occupation: { type: String, required: false },
-  ageRange: { type: String, required: false },
-  isVoter: { type: String, required: false },
-  wantsPVC: { type: String, required: false },
-  state: { type: String, required: false },
-  lga: { type: String, required: false },
-  ward: { type: String, required: false },
-  wantsToVolunteer: { type: String, required: false },
-  vendor: { type: String, required: true, default: "Unknown" }, // Default value
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  sex: { type: String, required: true },
+  occupation: { type: String, required: true },
+  ageRange: { type: String, required: true },
+  isVoter: { type: String, required: true },
+  wantsPVC: { type: String },
+  state: { type: String, required: true },
+  lga: { type: String, required: true },
+  ward: { type: String, required: true },
+  wantsToVolunteer: { type: String, required: true },
+  vendor: { type: String, required: true },
   signedAt: { type: Date, default: Date.now },
 });
-
 
 export default mongoose.model("Petition", PetitionSchema);
